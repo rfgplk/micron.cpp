b69b1198914aeb5d875febe1f5bf751736c64949
rfgplk <rfgplk@users.noreply.github.com>
2026-02-11 21:21:54 +0100

    2/11, backport + hardening fixes

--- src/std.hpp
+++ src/std.hpp
@@ -25,7 +25,7 @@
 
 constexpr static const int MICRON_VERSION_MAJOR = 0x000;
 constexpr static const int MICRON_VERSION_MINOR = 0x050;
-constexpr static const int MICRON_VERSION_PATCH = 0x000;
+constexpr static const int MICRON_VERSION_PATCH = 0x010;
 
 #if defined(__clang__)
 #define COMPILER "Clang/LLVM"
