cee9b2237824e0ea6a60524d27127c3fae6591bb
rfgplk <rfgplk@users.noreply.github.com>
2026-06-13 12:05:43 +0200

    +bump ver

--- src/version.hpp
+++ src/version.hpp
@@ -17,7 +17,7 @@ constexpr static const int MICRON_ABI = 6;
 constexpr static const int MICRON_VERSION_MAJOR = 0x1000;
 constexpr static const int MICRON_VERSION_MINOR = 0x0600;
 constexpr static const int MICRON_VERSION_PATCH = 0x0040;
-constexpr static const int MICRON_VERSION_HOTFIX = 0x0002;
+constexpr static const int MICRON_VERSION_HOTFIX = 0x0003;
 
 template<int __major, int __minor, int __patch>
 constexpr bool
